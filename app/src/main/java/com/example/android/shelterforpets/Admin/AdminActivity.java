3ccddeb8e885e931178475045c71f73b98b46876
hazemelahwany <hazem.elahwany@gmail.com>
2017-04-25 04:24:10 +0200

    Main Functionality

--- app/src/main/java/com/example/android/shelterforpets/Admin/AdminActivity.java
+++ app/src/main/java/com/example/android/shelterforpets/Admin/AdminActivity.java
@@ -1,17 +1,30 @@
 package com.example.android.shelterforpets.Admin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.android.shelterforpets.Authentication.LogInActivity;
 import com.example.android.shelterforpets.R;
+import com.example.android.shelterforpets.User.UserMainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.example.android.shelterforpets.Authentication.LogInActivity.mFirebaseAuth;
 
 
 public class AdminActivity extends AppCompatActivity {
 
+    FirebaseAuth.AuthStateListener mAuthStateListener;
 
+    private static final int RC_SIGNIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,7 @@ public class AdminActivity extends AppCompatActivity {
         Button addShelters = (Button) findViewById(R.id.add_shelter);
         Button vets = (Button) findViewById(R.id.admin_vets);
         Button events = (Button) findViewById(R.id.admin_events);
+        Button signout = (Button) findViewById(R.id.admin_sign_out);
 
         addAdmins.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +64,54 @@ public class AdminActivity extends AppCompatActivity {
                 startActivity(new Intent(AdminActivity.this, AddEvents.class));
             }
         });
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LogInActivity.mFirebaseAuth.signOut();
+            }
+        });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                        Toast.makeText(AdminActivity.this, "User signed in", Toast.LENGTH_SHORT)
+                                .show();
+                } else {
+                    // User is signed out
+                    Log.d("Authentication", "onAuthStateChanged:signed_out");
+                    startActivityForResult(new Intent(AdminActivity.this, LogInActivity.class),
+                            RC_SIGNIN);
+                }
+            }
+        };
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGNIN) {
+            if (resultCode == RESULT_OK) {
+                Log.v("signin", "signed in");
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.v("signin", "signed in cancelled");
+                finish();
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/android/shelterforpets/User/UserMainActivity.java
+++ app/src/main/java/com/example/android/shelterforpets/User/UserMainActivity.java
@@ -1,16 +1,11 @@
 package com.example.android.shelterforpets.User;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,11 +17,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.shelterforpets.Authentication.LogInActivity;
+import com.example.android.shelterforpets.DatabaseObjects.LostPet;
 import com.example.android.shelterforpets.R;
+import com.example.android.shelterforpets.TrackGPS;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+import java.util.UUID;
 
 import static com.example.android.shelterforpets.Authentication.LogInActivity.mFirebaseAuth;
 
@@ -38,12 +50,17 @@ public class UserMainActivity extends AppCompatActivity {
 
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-    // Acquire a reference to the system Location Manager
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    private String loc;
+
+    private StorageReference lostPetsRef;
+    private DatabaseReference sheltersDatabase;
+
+
     private double longitude;
     private double latitude;
+    private TrackGPS gps;
+    private Location currentLocation;
+    private float minimumDistance;
+    private String shelterID;
 
 
     private ImageView takenPic;
@@ -54,33 +71,61 @@ public class UserMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
 
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        sheltersDatabase = mFirebaseDatabase.getReference().child("Shelters");
+        lostPetsRef = storage.getReference().child("lost_pets");
 
         Button signout = (Button) findViewById(R.id.signout_button);
         ImageButton captureAnimal = (ImageButton) findViewById(R.id.capture_animal);
         takenPic = (ImageView) findViewById(R.id.taken_pic);
         locationTv = (TextView) findViewById(R.id.location_tv);
 
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        // Define a listener that responds to location updates
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                longitude = location.getLongitude();
-                latitude = location.getLatitude();
-                loc = "Longitude: " + longitude + "\n"
-                        + "Latitude: " + latitude;
-                locationTv.setText(loc);
+        gps = new TrackGPS(UserMainActivity.this);
+        currentLocation = new Location("");
+        minimumDistance = 0;
+
+        if (gps.canGetLocation()) {
+            longitude = gps.getLongitude();
+            latitude = gps.getLatitude();
+            currentLocation.setLatitude(latitude);
+            currentLocation.setLongitude(longitude);
+        } else {
+            gps.showSettingsAlert();
+        }
+
+        sheltersDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    for (DataSnapshot shelter : dataSnapshot.getChildren()) {
+                        String location = shelter.child("shelterAddress").getValue(String.class);
+                        double lng = Double.parseDouble(location.split(",")[0]);
+                        double lat = Double.parseDouble(location.split(",")[1]);
+                        Location shelterLocation = new Location("");
+                        shelterLocation.setLatitude(lat);
+                        shelterLocation.setLongitude(lng);
+                        float currentDistance = shelterLocation.distanceTo(currentLocation);
+                        if (minimumDistance == 0) {
+                            minimumDistance = currentDistance;
+                            shelterID = shelter.getKey();
+                        } else {
+                            if (currentDistance < minimumDistance) {
+                                minimumDistance = currentDistance;
+                                shelterID = shelter.getKey();
+                            }
+                        }
+                    }
+                }
             }
 
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-            public void onProviderEnabled(String provider) {
             }
+        });
 
-            public void onProviderDisabled(String provider) {
-            }
-        };
 
         signout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -160,18 +205,34 @@ public class UserMainActivity extends AppCompatActivity {
                         }
                     });
         } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: 17-04-2017 check if location enabled
-                Toast.makeText(this, "Check your Location and Network Settings", Toast.LENGTH_LONG).show();
-            } else {
-                String locationProvider = LocationManager.NETWORK_PROVIDER;
-                locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] dataArray = baos.toByteArray();
 
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                takenPic.setImageBitmap(imageBitmap);
-            }
+            final String uuid = UUID.randomUUID().toString();
+            StorageReference photoRef = lostPetsRef.child(uuid);
+            UploadTask uploadTask = photoRef.putBytes(dataArray);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    LostPet lostPet = new LostPet(downloadUrl.toString(), "" + longitude + "," + latitude);
+                    sheltersDatabase.child(shelterID).child("Requests").child(uuid).setValue(lostPet);
+                }
+            });
+            takenPic.setImageBitmap(imageBitmap);
+            String loc = "Longitude: " + longitude + "\n"
+                    + "Latitude: " + latitude;
+            locationTv.setText(loc);
+
 
 
         }
